12872f7bf43c3f2f0346fd7bd564c5550c026f7c
GuilhermePFA <guilhermepadilhafreirealves@gmail.com>
2026-04-21 11:04:23 -0700

    feat: US-005 - Endpoint de status do processamento

--- server/src/schemas/video.py
+++ server/src/schemas/video.py
@@ -6,3 +6,9 @@ from pydantic import BaseModel
 class VideoUploadResponse(BaseModel):
     video_id: int
     status: str
+
+
+class VideoStatusResponse(BaseModel):
+    video_id: int
+    status: str
+    status_descricao: str
